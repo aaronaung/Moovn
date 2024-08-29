589e65b94d1b26a85d4f0fc1a5a6f93eb256275f
Aaron Aung <aaronaung.95@gmail.com>
2024-08-28 17:16:12 -0700

    comment unused stripe code for now to pass build

--- app/api/stripe/webhook/route.ts
+++ app/api/stripe/webhook/route.ts
@@ -2,37 +2,33 @@ import { NextRequest } from "next/server";
 import { stripeClient } from "..";
 import Stripe from "stripe";
 
-import { supaServerClient } from "@/src/data/clients/server";
-import { throwOrData } from "@/src/data/util";
 import { StripeCheckoutMetadata } from "../check-out";
-import { ProductType } from "../products";
 
 const handleProductPurchase = async (metadata: StripeCheckoutMetadata) => {
-  let quantity = parseInt(metadata.quantity) || 0;
-  if (metadata.product_type === ProductType.Review) {
-    // Review products are not limited to one per user.
-    const result = await throwOrData(
-      supaServerClient()
-        .from("user_stripe_products")
-        .select("quantity")
-        .eq("stripe_product_id", metadata.stripe_product_id)
-        .eq("user_id", metadata.user_id)
-        .eq("type", metadata.product_type)
-        .maybeSingle(),
-    );
-    if (result) {
-      quantity += result.quantity;
-    }
-  }
-
-  await throwOrData(
-    supaServerClient().from("user_stripe_products").upsert({
-      stripe_product_id: metadata.stripe_product_id,
-      user_id: metadata.user_id,
-      type: metadata.product_type,
-      quantity,
-    }),
-  );
+  // let quantity = parseInt(metadata.quantity) || 0;
+  // if (metadata.product_type === ProductType.Review) {
+  //   // Review products are not limited to one per user.
+  //   const result = await throwOrData(
+  //     supaServerClient()
+  //       .from("user_stripe_products")
+  //       .select("quantity")
+  //       .eq("stripe_product_id", metadata.stripe_product_id)
+  //       .eq("user_id", metadata.user_id)
+  //       .eq("type", metadata.product_type)
+  //       .maybeSingle(),
+  //   );
+  //   if (result) {
+  //     quantity += result.quantity;
+  //   }
+  // }
+  // await throwOrData(
+  //   supaServerClient().from("user_stripe_products").upsert({
+  //     stripe_product_id: metadata.stripe_product_id,
+  //     user_id: metadata.user_id,
+  //     type: metadata.product_type,
+  //     quantity,
+  //   }),
+  // );
 };
 
 export async function POST(req: NextRequest) {
@@ -58,7 +54,9 @@ export async function POST(req: NextRequest) {
       const checkoutSessionCompleteEvent = event.data.object;
       if (checkoutSessionCompleteEvent.mode === "payment") {
         // This handles free products.
-        await handleProductPurchase(checkoutSessionCompleteEvent.metadata as unknown as StripeCheckoutMetadata);
+        await handleProductPurchase(
+          checkoutSessionCompleteEvent.metadata as unknown as StripeCheckoutMetadata,
+        );
       }
 
       console.log("checkout.session.completed", checkoutSessionCompleteEvent);
@@ -68,7 +66,9 @@ export async function POST(req: NextRequest) {
       break;
     case "payment_intent.succeeded":
       const paymentSuccessEvent = event.data.object;
-      await handleProductPurchase(paymentSuccessEvent.metadata as unknown as StripeCheckoutMetadata);
+      await handleProductPurchase(
+        paymentSuccessEvent.metadata as unknown as StripeCheckoutMetadata,
+      );
       break;
     case "payment_intent.payment_failed":
       break;
--- src/data/stripe.ts
+++ src/data/stripe.ts
@@ -1,188 +1,188 @@
-import { CheckOutRequest } from "@/app/api/stripe/check-out/dto/check-out.dto";
-import { UpsertStripeProductRequest } from "@/app/api/stripe/products/dto/upsert-product.dto";
-import Stripe from "stripe";
-import { throwOrData } from "./util";
-import { SupabaseOptions } from "./clients/types";
-import { CreateSubscriptionRequest } from "@/app/api/stripe/subscriptions/dto/create.dto";
-import { CancelSubscriptionRequest } from "@/app/api/stripe/subscriptions/dto/cancel.dto";
-import { UpdateSubscriptionRequest } from "@/app/api/stripe/subscriptions/dto/update.dto";
-import { SetupPaymentRequest } from "@/app/api/stripe/payment-methods/dto/setup.dto";
-import { CreateCustomerProfileRequest } from "@/app/api/stripe/onboard/dto/create-customer-profile.dto";
+// import { CheckOutRequest } from "@/app/api/stripe/check-out/dto/check-out.dto";
+// import { UpsertStripeProductRequest } from "@/app/api/stripe/products/dto/upsert-product.dto";
+// import Stripe from "stripe";
+// import { throwOrData } from "./util";
+// import { SupabaseOptions } from "./clients/types";
+// import { CreateSubscriptionRequest } from "@/app/api/stripe/subscriptions/dto/create.dto";
+// import { CancelSubscriptionRequest } from "@/app/api/stripe/subscriptions/dto/cancel.dto";
+// import { UpdateSubscriptionRequest } from "@/app/api/stripe/subscriptions/dto/update.dto";
+// import { SetupPaymentRequest } from "@/app/api/stripe/payment-methods/dto/setup.dto";
+// import { CreateCustomerProfileRequest } from "@/app/api/stripe/onboard/dto/create-customer-profile.dto";
 
-export const getUserStripeProduct = async (
-  {
-    userId,
-    stripeProductId,
-  }: {
-    userId: string;
-    stripeProductId?: string | null;
-  },
-  { client }: SupabaseOptions,
-) => {
-  if (!stripeProductId) {
-    return null;
-  }
+// export const getUserStripeProduct = async (
+//   {
+//     userId,
+//     stripeProductId,
+//   }: {
+//     userId: string;
+//     stripeProductId?: string | null;
+//   },
+//   { client }: SupabaseOptions,
+// ) => {
+//   if (!stripeProductId) {
+//     return null;
+//   }
 
-  return throwOrData(
-    client
-      .from("user_stripe_products")
-      .select("*")
-      .eq("stripe_product_id", stripeProductId)
-      .eq("user_id", userId)
-      .maybeSingle(),
-  );
-};
+//   return throwOrData(
+//     client
+//       .from("user_stripe_products")
+//       .select("*")
+//       .eq("stripe_product_id", stripeProductId)
+//       .eq("user_id", userId)
+//       .maybeSingle(),
+//   );
+// };
 
-export const createStripeCustomerProfile = async (
-  req: CreateCustomerProfileRequest,
-): Promise<Stripe.Customer> => {
-  const resp = await fetch("/api/stripe/onboard/create-customer-profile", {
-    method: "POST",
-    body: JSON.stringify(req),
-  });
+// export const createStripeCustomerProfile = async (
+//   req: CreateCustomerProfileRequest,
+// ): Promise<Stripe.Customer> => {
+//   const resp = await fetch("/api/stripe/onboard/create-customer-profile", {
+//     method: "POST",
+//     body: JSON.stringify(req),
+//   });
 
-  return resp.json();
-};
+//   return resp.json();
+// };
 
-export const createStripeCheckoutSession = async (
-  req: CheckOutRequest,
-): Promise<Stripe.Checkout.Session> => {
-  const resp = await fetch("/api/stripe/check-out", {
-    method: "POST",
-    body: JSON.stringify(req),
-  });
+// export const createStripeCheckoutSession = async (
+//   req: CheckOutRequest,
+// ): Promise<Stripe.Checkout.Session> => {
+//   const resp = await fetch("/api/stripe/check-out", {
+//     method: "POST",
+//     body: JSON.stringify(req),
+//   });
 
-  return resp.json();
-};
+//   return resp.json();
+// };
 
-export const upsertStripeProduct = async (
-  req: UpsertStripeProductRequest,
-): Promise<Stripe.Product> => {
-  const resp = await fetch("/api/stripe/products/upsert", {
-    method: "POST",
-    body: JSON.stringify(req),
-  });
+// export const upsertStripeProduct = async (
+//   req: UpsertStripeProductRequest,
+// ): Promise<Stripe.Product> => {
+//   const resp = await fetch("/api/stripe/products/upsert", {
+//     method: "POST",
+//     body: JSON.stringify(req),
+//   });
 
-  return resp.json();
-};
+//   return resp.json();
+// };
 
-export const deleteStripeProduct = async (
-  stripeProductId: string,
-): Promise<void> => {
-  const resp = await fetch(`/api/stripe/products/${stripeProductId}`, {
-    method: "DELETE",
-  });
+// export const deleteStripeProduct = async (
+//   stripeProductId: string,
+// ): Promise<void> => {
+//   const resp = await fetch(`/api/stripe/products/${stripeProductId}`, {
+//     method: "DELETE",
+//   });
 
-  return resp.json();
-};
-export const getStripeAccount = async (
-  stripeAccountId: string,
-): Promise<Stripe.Account> => {
-  const resp = await fetch(`/api/stripe/accounts/${stripeAccountId}`);
-  return resp.json();
-};
+//   return resp.json();
+// };
+// export const getStripeAccount = async (
+//   stripeAccountId: string,
+// ): Promise<Stripe.Account> => {
+//   const resp = await fetch(`/api/stripe/accounts/${stripeAccountId}`);
+//   return resp.json();
+// };
 
-export const createStripeSubscription = async (
-  req: CreateSubscriptionRequest,
-): Promise<Stripe.Subscription> => {
-  const resp = await fetch("/api/stripe/subscriptions/create", {
-    method: "POST",
-    body: JSON.stringify(req),
-  });
+// export const createStripeSubscription = async (
+//   req: CreateSubscriptionRequest,
+// ): Promise<Stripe.Subscription> => {
+//   const resp = await fetch("/api/stripe/subscriptions/create", {
+//     method: "POST",
+//     body: JSON.stringify(req),
+//   });
 
-  return resp.json();
-};
+//   return resp.json();
+// };
 
-export const cancelStripeSubscription = async (
-  req: CancelSubscriptionRequest,
-): Promise<Stripe.Subscription> => {
-  const resp = await fetch(`/api/stripe/subscriptions/cancel`, {
-    method: "POST",
-    body: JSON.stringify(req),
-  });
+// export const cancelStripeSubscription = async (
+//   req: CancelSubscriptionRequest,
+// ): Promise<Stripe.Subscription> => {
+//   const resp = await fetch(`/api/stripe/subscriptions/cancel`, {
+//     method: "POST",
+//     body: JSON.stringify(req),
+//   });
 
-  return resp.json();
-};
+//   return resp.json();
+// };
 
-export const updateStripeSubscription = async (
-  req: UpdateSubscriptionRequest,
-): Promise<Stripe.Subscription> => {
-  const resp = await fetch(`/api/stripe/subscriptions/update`, {
-    method: "POST",
-    body: JSON.stringify(req),
-  });
+// export const updateStripeSubscription = async (
+//   req: UpdateSubscriptionRequest,
+// ): Promise<Stripe.Subscription> => {
+//   const resp = await fetch(`/api/stripe/subscriptions/update`, {
+//     method: "POST",
+//     body: JSON.stringify(req),
+//   });
 
-  return resp.json();
-};
+//   return resp.json();
+// };
 
-export const getStripeSubscriptionForBusiness = async (
-  businessId: string,
-  { client }: SupabaseOptions,
-): Promise<Stripe.Subscription | null> => {
-  const { stripe_subscription_id } = await throwOrData(
-    client.from("businesses").select("*").eq("id", businessId).single(),
-  );
-  if (!stripe_subscription_id) {
-    return null;
-  }
-  const resp = await fetch(
-    `/api/stripe/subscriptions/${stripe_subscription_id}`,
-  );
-  return resp.json();
-};
+// export const getStripeSubscriptionForBusiness = async (
+//   businessId: string,
+//   { client }: SupabaseOptions,
+// ): Promise<Stripe.Subscription | null> => {
+//   const { stripe_subscription_id } = await throwOrData(
+//     client.from("businesses").select("*").eq("id", businessId).single(),
+//   );
+//   if (!stripe_subscription_id) {
+//     return null;
+//   }
+//   const resp = await fetch(
+//     `/api/stripe/subscriptions/${stripe_subscription_id}`,
+//   );
+//   return resp.json();
+// };
 
-export const getStripeSetupPaymentSession = async (
-  req: SetupPaymentRequest,
-): Promise<Stripe.Checkout.Session> => {
-  const resp = await fetch(`/api/stripe/payment-methods/setup`, {
-    method: "POST",
-    body: JSON.stringify(req),
-  });
+// export const getStripeSetupPaymentSession = async (
+//   req: SetupPaymentRequest,
+// ): Promise<Stripe.Checkout.Session> => {
+//   const resp = await fetch(`/api/stripe/payment-methods/setup`, {
+//     method: "POST",
+//     body: JSON.stringify(req),
+//   });
 
-  return resp.json();
-};
+//   return resp.json();
+// };
 
-export const getStripePaymentMethods = async (
-  customerId?: string,
-): Promise<Stripe.PaymentMethod[]> => {
-  if (!customerId) {
-    return [];
-  }
-  const resp = await fetch(`/api/stripe/payment-methods/list`, {
-    method: "POST",
-    body: JSON.stringify({ customerId }),
-  });
+// export const getStripePaymentMethods = async (
+//   customerId?: string,
+// ): Promise<Stripe.PaymentMethod[]> => {
+//   if (!customerId) {
+//     return [];
+//   }
+//   const resp = await fetch(`/api/stripe/payment-methods/list`, {
+//     method: "POST",
+//     body: JSON.stringify({ customerId }),
+//   });
 
-  return resp.json();
-};
+//   return resp.json();
+// };
 
-export const deleteStripePaymentMethod = async (
-  paymentMethodId: string,
-): Promise<void> => {
-  const resp = await fetch(`/api/stripe/payment-methods/delete`, {
-    method: "POST",
-    body: JSON.stringify({ paymentMethodId }),
-  });
+// export const deleteStripePaymentMethod = async (
+//   paymentMethodId: string,
+// ): Promise<void> => {
+//   const resp = await fetch(`/api/stripe/payment-methods/delete`, {
+//     method: "POST",
+//     body: JSON.stringify({ paymentMethodId }),
+//   });
 
-  return resp.json();
-};
+//   return resp.json();
+// };
 
-export const releaseStripeSubscriptionSchedule = async (
-  subscriptionId: string,
-): Promise<Stripe.Subscription> => {
-  const resp = await fetch(
-    `/api/stripe/subscriptions/${subscriptionId}/release-schedule`,
-    {
-      method: "POST",
-    },
-  );
+// export const releaseStripeSubscriptionSchedule = async (
+//   subscriptionId: string,
+// ): Promise<Stripe.Subscription> => {
+//   const resp = await fetch(
+//     `/api/stripe/subscriptions/${subscriptionId}/release-schedule`,
+//     {
+//       method: "POST",
+//     },
+//   );
 
-  return resp.json();
-};
+//   return resp.json();
+// };
 
-export const getStripeDashboardLink = async (
-  accountId: string,
-): Promise<Stripe.LoginLink> => {
-  const resp = await fetch(`/api/stripe/accounts/${accountId}/dashboard-link`);
-  return resp.json();
-};
+// export const getStripeDashboardLink = async (
+//   accountId: string,
+// ): Promise<Stripe.LoginLink> => {
+//   const resp = await fetch(`/api/stripe/accounts/${accountId}/dashboard-link`);
+//   return resp.json();
+// };
