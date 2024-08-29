49465e5eedfa19dd5babb3fedece2dc3f5b6f450
Aaron Aung <aaronaung.95@gmail.com>
2024-08-28 17:42:39 -0700

    Add error messages for missing data for new users

--- app/(routes)/app/calendar/schedule-content/page.tsx
+++ app/(routes)/app/calendar/schedule-content/page.tsx
@@ -8,6 +8,34 @@ import { getTemplatesForAuthUser } from "@/src/data/templates";
 import { useSupaQuery } from "@/src/hooks/use-supabase";
 import { ArrowLeftIcon } from "@heroicons/react/24/outline";
 import { useRouter } from "next/navigation";
+import EmptyState from "@/src/components/common/empty-state";
+
+const MISSING_DETAILS = {
+  sources: {
+    title: "No sources found",
+    description: "Please create a source to continue.",
+    link: "/app/sources",
+    buttonText: "Go to Sources",
+  },
+  templates: {
+    title: "No templates found",
+    description: "Please create a template to continue.",
+    link: "/app/templates",
+    buttonText: "Go to Templates",
+  },
+  destinations: {
+    title: "No destinations found",
+    description: "Please create a destination to continue.",
+    link: "/app/destinations",
+    buttonText: "Go to Destinations",
+  },
+  connectedDestinations: {
+    title: "No connected destinations found",
+    description: "Please connect a destination to continue.",
+    link: "/app/destinations",
+    buttonText: "Go to Destinations",
+  },
+};
 
 export default function ScheduleContent() {
   const router = useRouter();
@@ -33,6 +61,31 @@ export default function ScheduleContent() {
   if (isLoadingSources || isLoadingTemplates || isLoadingDestinations) {
     return <Spinner className="mt-8" />;
   }
+
+  const connectedDestinations = (availableDestinations ?? []).filter(
+    (destination) => destination.linked_ig_user_id && destination.long_lived_token,
+  );
+
+  let missingDetails = null;
+
+  if ((connectedDestinations ?? []).length === 0)
+    missingDetails = MISSING_DETAILS.connectedDestinations;
+  if ((availableDestinations ?? []).length === 0) missingDetails = MISSING_DETAILS.destinations;
+  if ((availableTemplates ?? []).length === 0) missingDetails = MISSING_DETAILS.templates;
+  if ((availableSources ?? []).length === 0) missingDetails = MISSING_DETAILS.sources;
+
+  if (missingDetails) {
+    return (
+      <EmptyState
+        title={missingDetails.title}
+        description={missingDetails.description}
+        onAction={() => router.push(missingDetails.link)}
+        actionButtonText={missingDetails.buttonText}
+        actionButtonIcon={null}
+      />
+    );
+  }
+
   return (
     <div>
       <Button
@@ -45,7 +98,7 @@ export default function ScheduleContent() {
         <ArrowLeftIcon className="h-4 w-4" />
       </Button>
       <ContentSchedulingForm
-        availableDestinations={availableDestinations ?? []}
+        availableDestinations={connectedDestinations ?? []}
         availableSources={availableSources ?? []}
         availableTemplates={availableTemplates ?? []}
       />
